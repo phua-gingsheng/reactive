adcecc9f720ba21e0fbb30e1e611250fa8af95d9
phuagingsheng <phua.gingsheng@gmail.com>
2018-11-14 01:23:44 +0800

    Make publisher seen

--- src/test/java/TestReactive.java
+++ src/test/java/TestReactive.java
@@ -5,33 +5,38 @@ import org.junit.Test;
 
 import java.util.List;
 import java.util.concurrent.SubmissionPublisher;
+import java.util.concurrent.TimeUnit;
 
 public class TestReactive {
 
     @Test
-    public void testFlow() throws Exception {
-
-        int NUMBER_OF_MAGAZINES = 20;
-
+    public void testFlowA() throws Exception {
         final ReactiveFlowApp app = new ReactiveFlowApp();
-
-        System.out.println("\n\n### CASE 1: Subscribers are fast, buffer size is not so " +
-                "important in this case.");
+        System.out.println("\n\n### CASE 1: Subscribers are fast, buffer size is not " +
+                "so important in this case.");
         app.magazineDeliveryExample(100L, 100L, 8);
-
-        System.out.println("\n\n### CASE 2: A slow subscriber, but a good enough buffer " +
-                "size on the publisher's side to keep all items until they're picked up");
-        //app.magazineDeliveryExample(1000L, 3000L, NUMBER_OF_MAGAZINES);
-
-        System.out.println("\n\n### CASE 3: A slow subscriber, and a very limited buffer " +
-                "size on the publisher's side so it's important to keep the slow " +
-                "subscriber under control");
-        //app.magazineDeliveryExample(1000L, 3000L, 8);
-
     }
 
     @Test
-    public void testWhenSubscribeToItThenShouldConsumeAll() throws InterruptedException {
+    public void testFlowB() throws Exception {
+        int NUMBER_OF_MAGAZINES = 20;
+        final ReactiveFlowApp app = new ReactiveFlowApp();
+        System.out.println("\n\n### CASE 2: A slow subscriber, but a good enough buffer " +
+                "size on the publisher's side to keep all items until they're picked up");
+        app.magazineDeliveryExample(1000L, 3000L, NUMBER_OF_MAGAZINES);
+    }
+
+    @Test
+    public void testFlowC() throws Exception {
+        final ReactiveFlowApp app = new ReactiveFlowApp();
+        System.out.println("\n\n### CASE 3: A slow subscriber, and a very limited buffer " +
+                "size on the publisher's side so it's important to keep the slow " +
+                "subscriber under control");
+        app.magazineDeliveryExample(1000L, 3000L, 8);
+    }
+
+    @Test
+    public void testWhenSubscribeToItThenShouldConsumeAll() {
 
         // given
         SubmissionPublisher<String> publisher = new SubmissionPublisher<>();
@@ -42,9 +47,16 @@ public class TestReactive {
         transformProcessor.subscribe(subscriber);
 
         List<String> items = List.of("1", "2", "3", "4", "5", "6", "7", "8", "9", "10");
-        items.forEach(publisher::submit);
 
-        Thread.sleep(3000);
+        items.forEach(s -> {
+            try {
+                TimeUnit.MILLISECONDS.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            publisher.submit(s);
+        });
+
         publisher.close();
 
 
